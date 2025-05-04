4feb0abcbca68a30a9f9cb82ea6b9c0ea39d7af0
MOHAMED EL OUAFIR <mohamedoubaid999@gmail.com>
2025-05-04 21:06:25 +0100

    Player class implementation

---
+++ src/Controller/Player.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+/**
+ *
+ * @author moham
+ */
+public class Player {
+    
+    private String userName;
+    private String email;
+    
+    public Player(String userName, String email){
+        this.userName = userName;
+        this.email = email;
+    }
+}
